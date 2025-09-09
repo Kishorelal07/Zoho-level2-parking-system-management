598ed774e8f3b029ffe94840462e9ae4a68c0caf
kishorelal07 <arkishorelal@gmail.com>
2025-09-09 15:43:13 +0530

    Initial commit

---
+++ src/main/java/com/example/cycle/parking/system/CycleParkingSystemApplication.java
@@ -0,0 +1,17 @@
+package com.example.cycle.parking.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycleParkingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycleParkingSystemApplication.class, args);
+	}
+
+	@Override
+	public String toString() {
+		return super.toString();
+	}
+}
